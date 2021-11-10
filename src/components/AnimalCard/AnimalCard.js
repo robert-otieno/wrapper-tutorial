6ccf019dc941ea3cc6525b008ddda8b0aca79c9e
robert-otieno <robertotieno.or@gmail.com>
2021-11-10 08:50:17 +0300

    Creating Wrapper Components with children

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import Card from '../Card/Card'
 import AnimalDetails from '../AnimalDetails/AnimalDetails'
 
 export default function AnimalCard({ name, size, ...props }) {
   return(
-    <div>
+    <Card title="Animal">
       <h3>{name}</h3>
       <div>{size}kg</div>
       <AnimalDetails {...props} />
-    </div>
+    </Card>
   )
 }
 
